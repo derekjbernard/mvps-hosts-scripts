fbebc3ef0c69f0f2b8ab898fca725d3f9150257d
Derek J. Bernard <derekjbernard@gmail.com>
2015-02-23 09:29:56 -0500

    added mvps header detection and verifcation for curled text strips windows carriage returns (windows format) before appending to /etc/hosts.

--- rm-mvps-hosts.sh
+++ rm-mvps-hosts.sh
@@ -1,3 +1,28 @@
 #! /bin/bash
 
-sed -i.bak '/# This MVPS HOSTS file is a free download from:            #/,$d' /etc/hosts
+# mvps hosts first line of header
+mvpsheader='# This MVPS HOSTS file is a free download from:            #'
+
+# from /etc/hosts delete $mvpsheader to end of file leaving any prior entries
+# leaves an unmodified backup with .bak extension (backup required in os x sed)
+sed -i.bak "/$mvpsheader/,\$d" /etc/hosts
+
+
+# Get platform (for OS X this returns darwin)
+unamestr=`uname`
+
+# check if platform is Darwin (Apple Mac OS X) and if so clear dns cache
+if [[ "$unamestr" == 'Darwin' ]]; then
+	
+	# get OS X version add major version, e.g. OS X 10.10 = 10+10=20
+	osverint=`sw_vers -productVersion | awk -F '.' '{print $1 + $2}'`
+
+	# Yosemite clears dns cache differently than previous versions
+	# if greater or equal to yosemite (20) then use new way, else use old way
+	if [[ "$osverint" -ge 20 ]]; then
+		discoveryutil mdnsflushcache;
+ 	else
+		dscacheutil -flushcache
+		killall -HUP mDNSResponder;
+	fi
+fi
--- upd-hosts-mvps.sh
+++ upd-hosts-mvps.sh
@@ -1,4 +1,33 @@
 #! /bin/bash
 
-sed -i.bak '/# This MVPS HOSTS file is a free download from:            #/,$d' /etc/hosts
-curl http://winhelp2002.mvps.org/hosts.txt | sed $'s/\r//' >> /etc/hosts
+# mvps hosts first header line
+mvpsheader='# This MVPS HOSTS file is a free download from:            #'
+
+# from /etc/hosts delete $mvpsheader to end of file leaving any prior entries
+# leaves an unmodified backup with .bak extension (backup required in os x sed)
+sed -i.bak "/$mvpsheader/,\$d" /etc/hosts
+
+# download mvps hosts file, pipe to awk only printing from $mvpsheader to end of file 
+# this ensures $mvpsheader is present and will be the first line appended
+# remove any carrage return characters (stripping windows line endings)
+# append results to /etc/hosts
+curl http://winhelp2002.mvps.org/hosts.txt | awk "/^$mvpsheader/,0" | sed $'s/\r//' >> /etc/hosts
+
+# Get platform (e.g OS X = Darwin)
+unamestr=`uname`
+
+# check if platform is Darwin (Apple Mac OS X) and if so clear dns cache
+if [[ "$unamestr" == 'Darwin' ]]; then
+
+        # get OS X version add major version, e.g. OS X 10.10 = 10+10=20
+        osverint=`sw_vers -productVersion | awk -F '.' '{print $1 + $2}'`
+
+        # Yosemite clears dns cache differently than previous versions
+        # if greater or equal to yosemite (20) then use new way, else use old way
+        if [[ "$osverint" -ge 20 ]]; then
+                discoveryutil mdnsflushcache;
+        else
+                dscacheutil -flushcache
+                killall -HUP mDNSResponder;
+        fi
+fi
