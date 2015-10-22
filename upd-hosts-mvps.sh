66135ffe1bc7088e6f369f5126d3cd9b002a5a75
Derek J. Bernard <derekjbernard@gmail.com>
2015-10-22 16:26:01 -0400

    updated cache clearing to account for changes in 10.10.4

--- upd-hosts-mvps.sh
+++ upd-hosts-mvps.sh
@@ -16,16 +16,21 @@ curl http://winhelp2002.mvps.org/hosts.txt | awk "/^$mvpsheader/,0" | sed $'s/\r
 # Get platform (e.g OS X = Darwin)
 unamestr=`uname`
 
-# check if platform is Darwin (Apple Mac OS X) and if so clear dns cache
+# check if platform is Darwin (Apple Mac OS X) then clear dns cache
 if [[ "$unamestr" == 'Darwin' ]]; then
 
-        # get OS X version add major version, e.g. OS X 10.10 = 10+10=20
-        osverint=`sw_vers -productVersion | awk -F '.' '{print $1 + $2}'`
+        # get OS X major  and minor versions
+        osvermajor=`sw_vers -productVersion | awk -F '.' '{print $2}'`
+        osverminor=`sw_vers -productVersion | awk -F '.' '{print $3}'`
 
-        # Yosemite clears dns cache differently than previous versions
-        # if greater or equal to yosemite (20) then use new way, else use old way
-        if [[ "$osverint" -ge 20 ]]; then
-                discoveryutil mdnsflushcache;
+        # Yosemite 10.10.0 - 10.10.3 used discoveryutil to clears dns cache
+	# in 10.10.4 they went back to the methods used previously.
+        # previous versions used dscacheutil and / or mDNSResponder
+	# http://osxdaily.com/2014/11/20/flush-dns-cache-mac-os-x/
+	# http://osxdaily.com/2008/03/21/how-to-flush-your-dns-cache-in-mac-os-x/
+	if [[ "$osvermajor" == 10 ]] && [[ "$osverminor" -le 3 ]]; then
+			discoveryutil mdnsflushcache
+			discoveryutil udnsflushcaches
         else
                 dscacheutil -flushcache
                 killall -HUP mDNSResponder;
